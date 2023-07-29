63bdb827bb9cce92fa530db1572a44e677744a83
Nifdi Guliyev <87927314+Nifdi01@users.noreply.github.com>
2023-07-29 15:17:59 +0400

    Added pagination

--- blogvibe/blog/views.py
+++ blogvibe/blog/views.py
@@ -1,17 +1,46 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
+
+class PostListView(ListView):
+    """
+    Alternative to Function-base views:
+    - It gives better Organize code related to HTTP methods, such as 
+    GET, POST, or PUT, in separate methods, instead of using conditional branching
+    - Use multiple inheritance to create reusable view classes (also known as mixins)
+    """
+
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+
     return render(request,
                  'blog/post/list.html',
                  {'posts': posts})
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
-                             id=id, 
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day,
+                             slug=post,
                              status=Post.Status.PUBLISHED)
     return render(request,
                   'blog/post/detail.html',
---
+++ run.cmd
@@ -0,0 +1,1 @@
+python blogvibe/manage.py runserver 
